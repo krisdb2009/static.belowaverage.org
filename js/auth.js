e643e816ac3c32932faf2a807fff2408f740b8ad
Dylan K. Bickerstaff <dylan@belowaverage.org>
2018-03-06 19:22:55 -0500

    Added auth script

---
+++ js/auth.js
@@ -0,0 +1,22 @@
+var AUTH = {};
+AUTH.authenticated = null;
+AUTH.ready = false;
+AUTH.interval = null;
+AUTH.send = function() {
+	$('iframe[src=\'https://login.belowaverage.org/\']')[0].contentWindow.postMessage(undefined, 'https://login.belowaverage.org');
+};
+onmessage = function(e) {
+	if(!AUTH.ready && e.data == 'ready') {
+		clearInterval(AUTH.interval);
+		AUTH.ready = true;
+	} else if(AUTH.ready && e.data.length == 32) {
+		AUTH.token = e.data;
+		if(AUTH.authenticated !== null) {
+			AUTH.authenticated();
+		}
+		$('iframe[src=\'https://login.belowaverage.org/\']').remove();
+	}
+};
+$(document).ready(function() {
+	AUTH.interval = setInterval(AUTH.send, 100);
+});
