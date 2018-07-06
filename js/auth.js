d28800397b117b6616de55f4eb80de64cbf37fab
Dylan K. Bickerstaff <dylan@belowaverage.org>
2018-07-05 22:13:17 -0400

    Allow auth to relaunch on command.

--- js/auth.js
+++ js/auth.js
@@ -1,31 +1,30 @@
 var AUTH = {};
 AUTH.authenticated = function() {};
-AUTH.ready = false;
-AUTH.interval = null;
-AUTH.frame = null;
-AUTH.token = null;
-AUTH.send = function() {
-	var frames = document.getElementsByTagName('iframe');
-	var count = 0;
-	while(frames.length > count) {
-		AUTH.frame = frames[count++];
-		if(AUTH.frame.src == 'https://login.belowaverage.org/') {
-			AUTH.frame.contentWindow.postMessage(undefined, 'https://login.belowaverage.org');
-			break;
+AUTH.AUTH = function() {
+	AUTH.ready = false;
+	AUTH.frame = null;
+	AUTH.token = null;
+	AUTH.send = function() {
+		var frames = document.getElementsByTagName('iframe');
+		var count = 0;
+		while(frames.length > count) {
+			AUTH.frame = frames[count++];
+			if(AUTH.frame.src == 'https://login.belowaverage.org/' || AUTH.frame.src == 'https://login.belowaverage.org/#logout') {
+				AUTH.frame.contentWindow.postMessage(undefined, 'https://login.belowaverage.org');
+				break;
+			}
 		}
-	}
-	
-};
-onmessage = function(e) {
-	if(!AUTH.ready && e.data == 'ready') {
-		clearInterval(AUTH.interval);
-		AUTH.ready = true;
-	} else if(AUTH.ready && e.data.length == 32) {
-		AUTH.token = e.data;
-		AUTH.authenticated();
-		AUTH.frame.remove();
-	}
-};
-onload = function() {
+	};
+	onmessage = function(e) {
+		if(!AUTH.ready && e.data == 'ready') {
+			clearInterval(AUTH.interval);
+			AUTH.ready = true;
+		} else if(AUTH.ready && e.data.length == 32) {
+			AUTH.token = e.data;
+			AUTH.authenticated();
+			AUTH.frame.remove();
+		}
+	};
 	AUTH.interval = setInterval(AUTH.send, 100);
-};
+}
+AUTH.AUTH();
