8386821f11a1108c8499f5926529f0944f54e680
Dylan Bickerstaff <dylan@belowaverage.org>
2018-12-03 22:06:36 -0500

    Updated AUTH

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,8 @@
 var AUTH = {};
 AUTH.authenticated = function() {};
+AUTH.settings = {
+	removeIframe: true
+};
 AUTH.AUTH = function() {
 	AUTH.ready = false;
 	AUTH.frame = null;
@@ -19,13 +22,19 @@ AUTH.AUTH = function() {
 		if(!AUTH.ready && e.data == 'ready') {
 			clearInterval(AUTH.interval);
 			AUTH.ready = true;
-		} else if(AUTH.ready && e.data.length == 32) {
-			AUTH.token = e.data;
-			AUTH.authenticated();
-			AUTH.frame.remove();
+		} else {
+			if(AUTH.ready && e.data.length == 32) {
+				AUTH.token = e.data;
+				AUTH.authenticated();
+			} else if(AUTH.ready && typeof e.data == 'object' && typeof e.data.username == 'string' && typeof e.data.password == 'string') {
+				AUTH.credential = e.data;
+				AUTH.authenticated();
+			}
+			if(AUTH.settings.removeIframe) {
+				AUTH.frame.remove();
+			}
 		}
 	};
 	AUTH.interval = setInterval(AUTH.send, 5);
 }
-
 window.onload = AUTH.AUTH;
